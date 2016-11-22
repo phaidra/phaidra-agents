3e4e9beea243c84de7c4afe12c8be07bcfcdcaee
RastislavHudak <hudak.rastislav@gmail.com>
2016-11-22 10:25:36 +0100

    paths

--- apim-harvester/apim-daemon.pl
+++ apim-harvester/apim-daemon.pl
@@ -7,16 +7,16 @@ exit Daemon::Control->new(
     name        => "ActiveMQ Message Logger",
     lsb_start   => '$syslog $remote_fs',
     lsb_stop    => '$syslog',
-    lsb_sdesc   => 'Message Logger',
+    lsb_sdesc   => 'Phaidra agent: api-m message logger',
     lsb_desc    => 'Takes api-m messages and logs them to mongo.',
-    path        => '/etc/init.d/apim-harvester',
+    path        => '/usr/local/fedora/phaidra-agents/apim-harvester/apim-harvester',
  
-    program     => '/home/hudakr4/git/phaidra-agents/apim-harvester/apim-harvester.pl',
+    program     => '/usr/local/fedora/phaidra-agents/apim-harvester/apim-harvester.pl',
   #  program_args => [ '--debug' ], for debugging the harvester
  
-    pid_file    => '/tmp/apim-harvester.pid',
-    stderr_file => '/tmp/apim-harvester.out',
-    stdout_file => '/tmp/apim-harvester.out',
+    pid_file    => '/usr/local/fedora/phaidra-agents/apim-harvester/apim-harvester.pid',
+    stderr_file => '/var/log/phaidra/apim-harvester.out',
+    stdout_file => '/var/log/phaidra/apim-harvester.out',
  
     fork        => 2,
  
