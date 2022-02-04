c1fb151172a6dced6c3dbf062362580406df8f8f
RastislavHudak <hudak.rastislav@gmail.com>
2022-02-04 10:30:08 +0100

    monitoring config

--- apim-hooks/apim-hooks-check.pl
+++ apim-hooks/apim-hooks-check.pl
@@ -9,11 +9,17 @@ use Mojo::JSON 'from_json';
 use MIME::Lite;
 use Sys::Hostname;
 
+
 my $configfilepath = Mojo::File->new('/usr/local/phaidra/phaidra-agents/phaidra-agents.json');
 my $config = from_json $configfilepath->slurp;
 
+use lib '/usr/share/perl5';
+use lib '/usr/share/perl5/vendor_perl';
+use lib 'lib';
+use Net::Alertmanager;
+
 my ($phaidraapi_baseurl, $monitoring)= map { $config->{apimhooks}->{$_} } qw(phaidraapi_baseurl monitoring);
-my ($pidfile, $emailfrom, $emailto)= map { $monitoring->{$_} } qw(pidfile emailfrom emailto);
+my ($pidfile, $emailfrom, $emailto, $enableemail, $enablealert, $instance)= map { $monitoring->{$_} } qw(pidfile emailfrom emailto enableemail enablealert instance);
 
 # print "url=[$phaidraapi_baseurl] emailfrom=[$emailfrom] emailto=[$emailto]\n";
 
@@ -31,6 +37,8 @@ my $host = hostname;
 
 unless($alive){
 
+  if ($enableemail) # or option to send mail activated
+  {
     my $pwd = `pwd`; chop($pwd);
     my $msg = MIME::Lite->new(
 	  From     => $emailfrom,
@@ -40,7 +48,37 @@ unless($alive){
 	  Data     => "Phaidra API-M hooks not running for $phaidraapi_baseurl; please go to $pwd and restart!"
     );
 
-    $msg->send; 
+    $msg->send;
+  }
+
+  if ($enablealert) # or option to send alerts via alertmanager activated
+  {
+    my $alertmanager= new Net::Alertmanager (alertmanager => $config->{alertmanager});
+
+    my $alert=
+    {
+      labels =>
+      {
+        # required?
+        severity => 'page',
+        instance => 'apim-hooks-'.$instance,
+        alertname => 'apim-hooks-down',
+
+        # univie
+        # alert => 'gg', # who should receive the alert; if not present, defaults will apply
+      },
+      annotations =>
+      {
+        description => 'apim-hooks not running',
+        summary => 'apim-hooks not running',
+      },
+      # startsAt => "",
+      # endsAt => "",
+      # generatorURL => "",
+    };
+
+    $alertmanager->send_alerts( [ $alert ] );
+  }
 }
 
 1;
